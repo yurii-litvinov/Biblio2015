b24ea11fbf33d6cc5b3a5861419ec07470c326c9
Kudryavtsev Andrey <tmac1attack@gmail.com>
2016-02-29 21:27:46 +0300

    added 'GetScholarPage' controller
    also packages updated for vs 2015 Enterprise (from vs14)

---
+++ BootApp/BootApp/Controllers/ScholarController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+
+namespace BootApp.Controllers
+{
+    public class ScholarController : Controller
+    {
+        //
+        // GET: /Scholar/
+
+        public string GetScholarPage()
+        {
+            WebRequest req = WebRequest.Create("https://scholar.google.ru");
+            req.Method = "POST";
+            req.GetRequestStream();
+            string html;
+            using (HttpWebResponse response = (HttpWebResponse)req.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    html = reader.ReadToEnd();
+                }
+            }
+            StringWriter writer = new StringWriter();
+            return HttpUtility.HtmlEncode(html);
+        }
+    }
+}
