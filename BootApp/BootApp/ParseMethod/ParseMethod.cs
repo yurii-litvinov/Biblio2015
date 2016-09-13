76723991c35699cf11db880117d33ade7b26fa79
Кудрявцев Андрей <tmac1attack@gmail.com>
2016-09-13 22:18:44 +0300

    fixed XPath (update on scholar)
    GS updated class name on search result for some reason, it was changed
    from 'gs_ccl' to 'gs_ccl_results'

--- BootApp/BootApp/ParseMethod/ParseMethod.cs
+++ BootApp/BootApp/ParseMethod/ParseMethod.cs
@@ -39,27 +39,28 @@ namespace BootApp.Parsing
             for (int i = 1; i <= 10; i++)
             {
                 ScholarArticle article = new ScholarArticle();
-                string xPathBiblioCheck = string.Format("//*[@id='gs_ccl']/div[{0}]/div[2]", i);
+                string xPathBiblioCheck = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div[2]", i);
+                //*[@id="gs_ccl_results_results"]/div[1]
                 HtmlNode biblioCheck = doc.DocumentNode.SelectSingleNode(xPathBiblioCheck);
                 if (biblioCheck != null)
                 {
 
-                    string xPathRefCheck = string.Format("//*[@id='gs_ccl']/div[{0}]/div[2]/h3/a", i);
+                    string xPathRefCheck = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div[2]/h3/a", i);
                     HtmlNode refCheck = doc.DocumentNode.SelectSingleNode(xPathRefCheck);
 
                     // adding title of article 
-                    string xPathTitle = string.Format("//*[@id='gs_ccl']/div[{0}]/div[2]/h3/a", i);
+                    string xPathTitle = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div[2]/h3/a", i);
                     article.Title = doc.DocumentNode.SelectSingleNode(xPathTitle).InnerText;
 
                     // adding info 
-                    string xPathInfo = string.Format("//*[@id='gs_ccl']/div[{0}]/div[2]/div[1]", i);
+                    string xPathInfo = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div[2]/div[1]", i);
                     article.Info = doc.DocumentNode.SelectSingleNode(xPathInfo).InnerText;
 
                     // adding reference
                     article.Reference = refCheck.GetAttributeValue("href", null);
 
                     // adding citiations amount
-                    string xPathCitiations = string.Format("//*[@id='gs_ccl']/div[{0}]/div[2]/div[3]/a[1]", i);
+                    string xPathCitiations = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div[2]/div[3]/a[1]", i);
                     string citiationsCheck = doc.DocumentNode.SelectSingleNode(xPathCitiations).InnerText;
                     if (citiationsCheck.StartsWith("Цитируется"))
                         article.Citiations = citiationsCheck;
@@ -70,19 +71,19 @@ namespace BootApp.Parsing
                 }
                 else
                 {
-                    string xPathRefCheck = string.Format("//*[@id='gs_ccl']/div[{0}]/div/h3/a", i);
+                    string xPathRefCheck = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div/h3/a", i);
                     HtmlNode refCheck = doc.DocumentNode.SelectSingleNode(xPathRefCheck);
                     if (refCheck != null)
                     {
-                        string xPathTitle = string.Format("//*[@id='gs_ccl']/div[{0}]/div/h3/a", i);
+                        string xPathTitle = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div/h3/a", i);
                         article.Title = doc.DocumentNode.SelectSingleNode(xPathTitle).InnerText;
 
-                        string xPathInfo = string.Format("//*[@id='gs_ccl']/div[{0}]/div/div[1]", i);
+                        string xPathInfo = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div/div[1]", i);
                         article.Info = doc.DocumentNode.SelectSingleNode(xPathInfo).InnerText;
 
                         article.Reference = refCheck.GetAttributeValue("href", null);
 
-                        string xPathCitiations = string.Format("//*[@id='gs_ccl']/div[{0}]/div/div[3]/a[1]", i);
+                        string xPathCitiations = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div/div[3]/a[1]", i);
                         string citiationsCheck = doc.DocumentNode.SelectSingleNode(xPathCitiations).InnerText;
                         if (citiationsCheck.StartsWith("Цитируется"))
                             article.Citiations = citiationsCheck;
@@ -95,19 +96,19 @@ namespace BootApp.Parsing
                     else
                     {
                         // case, when article do not has reference, but has a tag [citiation]
-                        string xPathTitleCheck = string.Format("//*[@id='gs_ccl']/div[{0}]/div/h3", i);
-                        string xPathSpanNode = string.Format("//*[@id='gs_ccl']/div[{0}]/div/h3/span", i);
+                        string xPathTitleCheck = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div/h3", i);
+                        string xPathSpanNode = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div/h3/span", i);
                         HtmlNode titleMatchNode = doc.DocumentNode.SelectSingleNode(xPathTitleCheck);
                         HtmlNode spanNode = doc.DocumentNode.SelectSingleNode(xPathSpanNode);
                         titleMatchNode.RemoveChild(spanNode);
                         article.Title = titleMatchNode.InnerText;
 
-                        string xPathInfo = string.Format("//*[@id='gs_ccl']/div[{0}]/div/div[1]", i);
+                        string xPathInfo = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div/div[1]", i);
                         article.Info = doc.DocumentNode.SelectSingleNode(xPathInfo).InnerText;
 
                         article.Reference = "This article does not have a reference";
 
-                        string xPathCitiations = string.Format("//*[@id='gs_ccl']/div[{0}]/div/div[2]/a[1]", i);
+                        string xPathCitiations = string.Format("//*[@id='gs_ccl_results']/div[{0}]/div/div[2]/a[1]", i);
                         string citiationsCheck = doc.DocumentNode.SelectSingleNode(xPathCitiations).InnerText;
                         if (citiationsCheck.StartsWith("Цитируется"))
                             article.Citiations = citiationsCheck;
