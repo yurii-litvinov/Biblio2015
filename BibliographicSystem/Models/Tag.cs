6d873cd2af861eeffbcf365c65e933f75642b109
Ivan <moolush671@gmail.com>
2016-11-13 12:46:05 +0300

    Packages update
    All NuGet packages and database was updated. Small UI fixes.

---
+++ BibliographicSystem/Models/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace BibliographicSystem.Models
+{
+    public sealed class Tag
+    {
+        public int TagId { get; set; }
+        public string TagName { get; set; }
+
+        public ICollection<Article> Articles { get; set; }
+        public Tag()
+        {
+            Articles = new List<Article>();
+        }
+    }
+}
---
+++ BibliographicSystem/Models/UsersArticle.cs
@@ -0,0 +1,9 @@
+﻿namespace BibliographicSystem.Models
+{
+    public class UsersArticle
+    {
+        public int Id { get; set; }
+        public int ArticleId { get; set; }
+        public string UserName { get; set; }
+    }
+}
