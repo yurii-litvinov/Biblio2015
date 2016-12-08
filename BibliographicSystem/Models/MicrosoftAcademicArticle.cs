cdb0fdd79299acb05011b31bd22c6f7e58e7f126
AlbertMukhammadiev <beehead02@gmail.com>
2016-12-08 21:48:51 +0400

    some renames and changes in model

--- BibliographicSystem/Models/MicrosoftAcademicArticle.cs
+++ BibliographicSystem/Models/MicrosoftAcademicArticle.cs
@@ -1,9 +1,18 @@
-﻿namespace BibliographicSystem.Models
+﻿using System.Collections.Generic;
+
+namespace BibliographicSystem.Models
 {
     public class MicrosoftAcademicArticle
     {
-        public string Title { get; set; }
-        public int Year { get; set; }
-        public string Author { get; set; }
+        public string PaperTitle { get; set; }
+        public int PaperYear { get; set; }
+        public int CitationCount { get; set; }
+        public List<Author> Authors { get; set; }
+    }
+
+    public class Author
+    {
+        public string AuthorName { get; set; }
+        public long AuthorId { get; set; }
     }
 }
