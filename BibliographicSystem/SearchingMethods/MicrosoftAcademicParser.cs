aa46087212d1253c737f777ad1153c0b9ac0c0ee
AlbertMukhammadiev <beehead02@gmail.com>
2016-12-14 21:28:13 +0300

    class for copy information and new comments were added

--- BibliographicSystem/SearchingMethods/MicrosoftAcademicParser.cs
+++ BibliographicSystem/SearchingMethods/MicrosoftAcademicParser.cs
@@ -27,7 +27,7 @@ namespace BibliographicSystem.SearchingMethods
         public MicrosoftAcademicParser(UserQuery userQuery)
         {
             this.userQuery = userQuery;
-            rootObject = new RootObject();
+            rootObject = new Response();
         }
 
         #region классы для десериализации json
@@ -43,59 +43,107 @@ namespace BibliographicSystem.SearchingMethods
             public string Ti { get; set; }
             public int Y { get; set; }
             public int CC { get; set; }
+            public string E { get; set; }
             public List<AA> AA { get; set; }
         }
 
-        public class RootObject
+        public class Response
         {
             public string expr { get; set; }
             public List<Entity> entities { get; set; }
         }
 
-        public class RootConverter : Newtonsoft.Json.Converters.CustomCreationConverter<RootObject>
+        public class ResponseConverter : Newtonsoft.Json.Converters.CustomCreationConverter<Response>
         {
-            public override RootObject Create(Type objectType)
+            public override Response Create(Type objectType)
             {
-                return new RootObject();
+                return new Response();
             }
+        }
+
+        public class MetadataConverter : Newtonsoft.Json.Converters.CustomCreationConverter<Extended>
+        {
+            public override Extended Create(Type objectType)
+            {
+                return new Extended();
+            }
+        }
+
+        /// <summary>
+        /// for deserialization extended metadata
+        /// </summary>
+        public class S
+        {
+            public int Ty { get; set; }
+            public string U { get; set; }
+        }
+
+        /// <summary>
+        /// for deserialization extended metadata
+        /// </summary>
+        public class Extended
+        {
+            public string DN { get; set; }
+            public string D { get; set; }
+            public IList<S> S { get; set; }
+            public string VFN { get; set; }
+            public int V { get; set; }
+            public int I { get; set; }
+            public int FP { get; set; }
+            public int LP { get; set; }
+            public string DOI { get; set; }
         }
 
         #endregion
 
+        /// <summary>
+        /// a class for storing precise user request
+        /// </summary>
         public class UserQuery
         {
             public string MainInput { get; set; }
             public string Authors { get; set; }
             public string Year { get; set; }
+            public string Count { get; set; }
         }
 
+        /// <summary>
+        /// returns list of articles
+        /// </summary>
+        /// <returns></returns>
         public List<MicrosoftAcademicArticle> GetSearchResult()
         {
             var listOfArticles = new List<MicrosoftAcademicArticle>();
-            MakeGetRequest();
-            foreach (var element in rootObject.entities)
+            var expressions = GetListOfExpr();
+            var count = (userQuery.Count == ""? "10" : userQuery.Count);
+            foreach (var expression in expressions)
             {
-                var article = new MicrosoftAcademicArticle
-                {
-                    PaperTitle = element.Ti,
-                    PaperYear = element.Y,
-                    CitationCount = element.CC
-                };
-                article.Authors = new List<Author>();
 
-                foreach (var author in element.AA)
+                count = (Convert.ToInt32(count) - listOfArticles.Count).ToString();
+                MakeGetRequest(expression, count);
+
+                foreach (var element in rootObject.entities)
                 {
-                    article.Authors.Add(new Author { AuthorName = author.AuN, AuthorId = author.AuId } );
+                    var article = CopyData(element);
+                    listOfArticles.Add(article);
                 }
 
-                listOfArticles.Add(article);
+                if (listOfArticles.Count.ToString() == userQuery.Count)
+                {
+                    break;
+                }
             }
-
+            
             return listOfArticles;
 
         }
 
-        public void MakeGetRequest()
+        /// <summary>
+        /// makes get request to Microsoft Academic
+        /// </summary>
+        /// <param name="expression">valid query string</param>
+        /// <param name="count">the number of articles, that the user wants to see</param>
+        private void MakeGetRequest(string expression, string count)
         {
             var client = new HttpClient();
             var queryString = HttpUtility.ParseQueryString(string.Empty);
@@ -104,12 +152,12 @@ namespace BibliographicSystem.SearchingMethods
             client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "a89fcc82bd8049738b2f76c667f0f52c");
 
             // Request parameters
-            queryString["expr"] = GetExpression();
+            queryString["expr"] = expression;
             queryString["model"] = "latest";
-            queryString["count"] = "10";
+            queryString["count"] = count;
             queryString["offset"] = "0";
             queryString["orderby"] = "";
-            queryString["attributes"] = "Ti,Y,AA.AuN,AA.AuId,CC";
+            queryString["attributes"] = "Ti,Y,AA.AuN,AA.AuId,CC,E";
             var uri = "https://api.projectoxford.ai/academic/v1.0/evaluate?" + queryString;
 
             var response = client.GetAsync(uri).Result;
@@ -117,11 +165,15 @@ namespace BibliographicSystem.SearchingMethods
             if (response.StatusCode == HttpStatusCode.OK)
             {
                 var json = response.Content.ReadAsStringAsync().Result;
-                rootObject = JsonConvert.DeserializeObject<RootObject>(json, new RootConverter());
+                rootObject = JsonConvert.DeserializeObject<Response>(json, new ResponseConverter());
             }
         }
 
-        private string GetExpression()
+        /// <summary>
+        /// returns the list of query string which are not contrary Query Expression Syntax
+        /// </summary>
+        /// <returns></returns>
+        private List<string> GetListOfExpr()
         {
             var listOfExpr = new List<string>();
             var expression = "And(";
@@ -136,13 +188,13 @@ namespace BibliographicSystem.SearchingMethods
                 }
 
                 expression = expression.Remove(expression.Length - 1, 1);
-                expression += ")";
+                expression += "),";
             }
             
             if (userQuery.MainInput.Length != 0)
             {
                 var inputtedWords = userQuery.MainInput.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                expression += ",Or(";
+                expression += "Or(";
                 foreach (var word in inputtedWords)
                 {
                     expression += "W='" + word + "',";
@@ -151,18 +203,61 @@ namespace BibliographicSystem.SearchingMethods
                 expression = expression.Remove(expression.Length - 1, 1);
                 expression += ")";
             }
-            
+
             if (userQuery.Year.Length != 0)
             {
                 expression += ",Y=" + userQuery.Year;
             }
 
             expression += ")";
-            return expression;
+            listOfExpr.Add(expression);
+
+            /// replace "And" to "Or"
+            expression = "Or" + expression.Remove(0, 3);
+            listOfExpr.Add(expression);
+
+            return listOfExpr;
         }
 
+        /// <summary>
+        /// creates an article, copies the required data from deserialized object, and returns this article
+        /// </summary>
+        /// <param name="entity">deserialized object</param>
+        /// <returns></returns>
+        private MicrosoftAcademicArticle CopyData(Entity entity)
+        {
+            Extended extendedMetadata = JsonConvert.DeserializeObject<Extended>(entity.E, new MetadataConverter());
+            var article = new MicrosoftAcademicArticle
+            {
+                Year = entity.Y,
+                CitationCount = entity.CC,
+                ExtendedMetadata = entity.E,
+                Title = extendedMetadata.DN,
+            };
+
+            article.References = new List<string>();
+            if (extendedMetadata.S != null)
+            {
+                foreach (var reference in extendedMetadata.S)
+                {
+                    article.References.Add(reference.U);
+                }
+            }
+
+            article.Authors = new List<Author>();
+            if (entity.AA != null)
+            {
+                foreach (var author in entity.AA)
+                {
+                    article.Authors.Add(new Author { AuthorName = author.AuN, AuthorId = author.AuId });
+                }
+            }
+
+            article.Description = extendedMetadata.D == null ? "¯\\_(ツ)_/¯" : extendedMetadata.D;
+            return article;
+        }
 
         private UserQuery userQuery;
-        private RootObject rootObject;
+        private Response rootObject;
     }
 }
