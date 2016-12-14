77c737722fe2ad0a9b737a0556bcaa359317b7c9
AlbertMukhammadiev <beehead02@gmail.com>
2016-12-14 21:25:42 +0300

    new information about article was added

--- BibliographicSystem/Models/MicrosoftAcademicArticle.cs
+++ BibliographicSystem/Models/MicrosoftAcademicArticle.cs
@@ -4,9 +4,12 @@ namespace BibliographicSystem.Models
 {
     public class MicrosoftAcademicArticle
     {
-        public string PaperTitle { get; set; }
-        public int PaperYear { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
         public int CitationCount { get; set; }
+        public string ExtendedMetadata { get; set; }
+        public List<string> References { get; set; }
         public List<Author> Authors { get; set; }
     }
 
