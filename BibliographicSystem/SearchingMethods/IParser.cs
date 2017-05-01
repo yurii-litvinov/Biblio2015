95dbb456732146683a8bb8a64cac0a4c6d1b614f
AlbertMukhammadiev <beehead02@gmail.com>
2017-05-01 11:58:05 +0300

    interface for search parsers

---
+++ BibliographicSystem/SearchingMethods/IParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json;
+using BibliographicSystem.Models;
+using System.Linq;
+
+namespace BibliographicSystem.SearchingMethods
+{
+    interface IParser
+    {
+        List<OutsideArticle> GetArticles();
+
+        UserQuery Query { get; set; }
+    }
+}
